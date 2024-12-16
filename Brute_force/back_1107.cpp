05f49b4636e6c5cb0029a5d2183f3210a31d5e3d
June Hyck Lee <132459407+happylee8282@users.noreply.github.com>
2024-12-16 22:39:58 +0900

    Create back_1107.cpp

---
+++ Brute_force/back_1107.cpp
@@ -0,0 +1,65 @@
+/*
+문제
+수빈이는 TV를 보고 있다. 수빈이는 채널을 돌리려고 했지만, 버튼을 너무 세게 누르는 바람에, 일부 숫자 버튼이 고장났다.
+
+리모컨에는 버튼이 0부터 9까지 숫자, +와 -가 있다. +를 누르면 현재 보고있는 채널에서 +1된 채널로 이동하고, -를 누르면 -1된 채널로 이동한다. 채널 0에서 -를 누른 경우에는 채널이 변하지 않고, 채널은 무한대 만큼 있다.
+
+수빈이가 지금 이동하려고 하는 채널은 N이다. 어떤 버튼이 고장났는지 주어졌을 때, 채널 N으로 이동하기 위해서 버튼을 최소 몇 번 눌러야하는지 구하는 프로그램을 작성하시오.
+
+수빈이가 지금 보고 있는 채널은 100번이다.
+
+입력
+첫째 줄에 수빈이가 이동하려고 하는 채널 N (0 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 고장난 버튼의 개수 M (0 ≤ M ≤ 10)이 주어진다. 고장난 버튼이 있는 경우에는 셋째 줄에는 고장난 버튼이 주어지며, 같은 버튼이 여러 번 주어지는 경우는 없다.
+
+출력
+첫째 줄에 채널 N으로 이동하기 위해 버튼을 최소 몇 번 눌러야 하는지를 출력한다.
+*/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+//정수형으로 10개의 값을 0으로 배정
+int broke_btn[10] = {
+	0,
+}; 
+
+bool whitch_btn(int n) { //전체 버튼이 들어옴
+	string str_n = to_string(n);
+	for (int i = 0; i < str_n.length(); i++) {
+		if (broke_btn[str_n[i] - '0'] == 1)
+			return false;
+	}
+	return true;
+}
+
+int main(void) {
+	int start = 100;
+	int in_1;
+	int in_2;
+	int fin;
+	cin >> in_1;
+
+	cin >> in_2;
+
+	for (int i = 0; i < in_2; i++) {
+		int in_3;
+		cin >> in_3;
+		broke_btn[in_3] = 1;
+	}
+
+	int count = abs(in_1 - start);
+	fin = count;
+
+	for (int i = 0 ; i <= 1000000; i++) {
+		if (whitch_btn(i) == true) {
+			int count_1 = to_string(i).length() + abs(in_1 - i);
+			fin = min(fin, count_1);
+		}
+	}
+
+	cout << fin <<endl;
+	return 0;
+}
